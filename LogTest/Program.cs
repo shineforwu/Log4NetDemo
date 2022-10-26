587da40579068931d85e44529cfd40a49d33c304
shine.wu <331046212@qq.com>
2022-10-26 09:50:34 +0800

    first commit

---
+++ LogTest/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LogCommon;
+
+namespace LogTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            log4net.Config.XmlConfigurator.Configure();
+            LogHelper.LogInfo("This msg is Info.");
+            LogHelper.LogError("This msg is Error.",new Exception("This my Exception"));
+        }
+    }
+}
